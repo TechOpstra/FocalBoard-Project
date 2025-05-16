423a0b223e299f9b7b94955a21e56e815c2bbc12
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 07:52:37 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "demo_tg" {
 
 resource "aws_lb_listener" "demo_listener" {
   load_balancer_arn = aws_lb.demo_alb.arn
-  port              = "8000"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
